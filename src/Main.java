a793e3ccaaef234579d4c101d04803b72fe9a970
AlexHitlere <dfdjffksj@gmail.comm>
2024-04-29 20:13:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,443 @@
+//1-exercise
+
+//public class Main {
+    //public static void main(String[] args) {
+       // a1(1);
+       // a2(0);
+        //a3(-3);
+    //}
+    //public static void a1(int a){
+      //  if(a==0){
+      //      System.out.println("Good");
+     //   }else{
+     //       System.out.println("Bad");
+     //   }
+   // }
+   // public static void a2(int a){
+    //    if(a==0){
+     //       System.out.println("Good");
+     //   }else{
+      //      System.out.println("Bad");
+      //  }
+   // }
+   // public static void a3(int a){
+       // if(a==0){
+      //      System.out.println("Good");
+     //   }else{
+     //       System.out.println("Bad");
+      //  }
+   // }
+//}
+//__________________________________________________________________________________________
+
+//2-exercise
+
+//public class Main {
+//public static void main(String[] args) {
+// a1(1);
+// a2(0);
+//a3(-3);
+//}
+//public static void a1(int a){
+//  if(a>0){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//   }
+// }
+// public static void a2(int a){
+//    if(a>0){
+//       System.out.println("Good");
+//   }else{
+//      System.out.println("Bad");
+//  }
+// }
+// public static void a3(int a){
+// if(a>0){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+// }
+//}
+//__________________________________________________________________________________________
+
+//3-exercise
+
+//public class Main {
+//public static void main(String[] args) {
+// a1(1);
+// a2(0);
+//a3(-3);
+//}
+//public static void a1(int a){
+//  if(a<0){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//   }
+// }
+// public static void a2(int a){
+//    if(a<0){
+//       System.out.println("Good");
+//   }else{
+//      System.out.println("Bad");
+//  }
+// }
+// public static void a3(int a){
+// if(a<0){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+// }
+//}
+//__________________________________________________________________________________________
+
+//4-exercise
+
+//public class Main {
+  //  public static void main(String[] args) {
+      //  a1(1);
+// a2(0);
+//a3(-3);
+    //}
+
+   // public static void a1(int a) {
+       // if(a>=0){
+          //  System.out.println("Good");
+       // }else
+          //  System.out.println("Bad");
+
+    //}
+// public static void a2(int a){
+//    if(a>=0){
+//       System.out.println("Good");
+//   }else{
+//      System.out.println("Bad");
+//  }
+// }
+// public static void a3(int a){
+// if(a>=0){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+// }
+//}
+//__________________________________________________________________________________________
+
+//5-exercise
+
+//public class Main {
+            //  public static void main(String[] args) {
+            //  a1(1);
+// a2(0);
+//a3(-3);
+//}
+
+// public static void a1(int a) {
+// if(a<=0){
+//  System.out.println("Good");
+// }else
+//  System.out.println("Bad");
+
+//}
+// public static void a2(int a){
+//    if(a<=0){
+//       System.out.println("Good");
+//   }else{
+//      System.out.println("Bad");
+//  }
+// }
+// public static void a3(int a){
+// if(a<=0){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+// }
+//}
+//__________________________________________________________________________________________
+
+//6-exercise
+
+//public class Main {
+//  public static void main(String[] args) {
+//  a1(1);
+// a2(0);
+//a3(-3);
+    //}
+
+// public static void a1(int a) {
+// if(a!=0){
+//  System.out.println("Good");
+// }else
+//  System.out.println("Bad");
+
+//}
+// public static void a2(int a){
+//    if(a!=0){
+//       System.out.println("Good");
+//   }else{
+//      System.out.println("Bad");
+//  }
+// }
+// public static void a3(int a){
+// if(a!=0){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+// }
+//}
+//__________________________________________________________________________________________
+
+//7-exercise
+
+//public class Main {
+  //public static void main(String[] args) {
+  //a1("test");
+  //a2("тест");
+//a3(3);
+    //}
+ //public static void a1(String a) {
+     //if (a == "test") {
+         //System.out.println("Good");
+     //} else {
+        // System.out.println("Bad");
+     //}
+ //}
+ //public static void a2(String a){
+    //if(a == "test"){
+   //    System.out.println("Good");
+  // }else{
+  //    System.out.println("Bad");
+ // }
+// }
+// public static void a3(String a){
+// if(a == "test"){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+// }
+//}
+//__________________________________________________________________________________________
+
+//8-exercise
+
+//public class Main {
+//public static void main(String[] args) {
+  //a1("1");
+  //a2(1);
+//a3(3);
+//}
+//public static void a1(String a) {
+//if (a == "1") {
+//System.out.println("Good");
+//} else {
+ //System.out.println("Bad");
+//}
+//}
+//public static void a2(String a){
+//if(a == "1"){
+    //System.out.println("Good");
+ //}else{
+    //System.out.println("Bad");
+ //}
+ //}
+// public static void a3(String a){
+// if(a == "1"){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+// }
+//}
+//__________________________________________________________________________________________
+
+//9-exercise
+
+///public class Main {
+    //public static void main(String[] args) {
+        //boolean bowlMiner = true;
+        //a1(true);
+        //a2(false);
+    //}
+   // public static void a1(boolean minKile){
+     //   if(minKile==true) {
+     //       System.out.println("True");
+     //   }else{
+     //       System.out.println("False");
+      //  }
+       // if(minKile) {
+      //      System.out.println("True");
+       // }else{
+       //     System.out.println("False");
+      //  }
+   // }
+   // public static void a2(boolean minKile){
+    //    if(minKile==true) {
+    //        System.out.println("True");
+    //    }else{
+     //       System.out.println("False");
+     //   }
+     //   if(minKile) {
+     //       System.out.println("True");
+     //   }else{
+     //       System.out.println("False");
+     //   }
+ //   }
+//}
+//__________________________________________________________________________________________
+
+//10-exercise
+
+///public class Main {
+//public static void main(String[] args) {
+//boolean bowlMiner = true;
+//a1(true);
+//a2(false);
+//}
+// public static void a1(boolean minKile){
+//   if(minKile!=true) {
+//       System.out.println("True");
+//   }else{
+//       System.out.println("False");
+//  }
+// if(minKile) {
+//      System.out.println("True");
+// }else{
+//     System.out.println("False");
+//  }
+// }
+// public static void a2(boolean minKile){
+//    if(minKile!=true) {
+//        System.out.println("True");
+//    }else{
+//       System.out.println("False");
+//   }
+//   if(minKile) {
+//       System.out.println("True");
+//   }else{
+//       System.out.println("False");
+//   }
+//   }
+//}
+//__________________________________________________________________________________________
+
+//11-exercise
+
+//public class Main {
+//public static void main(String[] args) {
+// a1(5);
+// a2(0);
+// a3(-3);
+// a4(2);
+//}
+//public static void a1(int a){
+//  if(a>0 && a<5){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//   }
+// }
+// public static void a2(int a){
+//    if(a>0 && a<5){
+//       System.out.println("Good");
+//   }else{
+//      System.out.println("Bad");
+//  }
+// }
+// public static void a3(int a){
+// if(a>0 && a<5){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+// }
+// public static void a4(int a){
+// if(a>0 && a<5){
+//      System.out.println("Good");
+//   }else{
+//       System.out.println("Bad");
+//  }
+//}
+//}
+//__________________________________________________________________________________________
+
+//12-exercise
+
+//public class Main {
+//public static void main(String[] args) {
+ //a1(5);
+ //a2(0);
+ //a3(-3);
+ //a4(2);
+//}
+//public static void a1(int a){
+  //if(a==0 || a==2){
+  //    System.out.println(a+7);
+  // }else{
+  //     System.out.println(a/10);
+ //  }
+// }
+// public static void a2(int a){
+  //   if(a==0 || a==2){
+   //      System.out.println(a+7);
+  //   }else{
+   //      System.out.println(a/10);
+   //  }
+ //}
+// public static void a3(int a){
+// if(a==0 || a==2){
+//      System.out.println(a+7);
+//   }else{
+//       System.out.println(a/10);
+//   }
+// }
+// public static void a4(int a){
+// if(a==0 || a==2){
+//      System.out.println(a+7);
+//   }else{
+//       System.out.println(a/10);
+//   }
+// }
+//}
+//__________________________________________________________________________________________
+
+//13-exercise
+
+//public class Main {
+    //public static void main(String[] args) {
+      //  a1(1, 3);
+
+
+//a2(0,6);
+//a3(3,5);
+   // }
+
+    //public static void a1(int a, int b) {
+      //  if (a <= 1 && b >= 3) {
+          //  System.out.println(a+b);
+       // }else{
+            //System.out.println(a-b);
+       // }
+   // }
+// public static void a2(int a){
+// if (a <= 1 && b >= 3) {
+//          //  System.out.println(a+b);
+//       // }else{
+//            //System.out.println(a-b);
+//       // }
+//}
+// public static void a3(int a){
+// if (a <= 1 && b >= 3) {
+//          //  System.out.println(a+b);
+//       // }else{
+//            //System.out.println(a-b);
+//       // }
+// }
+//}
+//__________________________________________________________________________________________
